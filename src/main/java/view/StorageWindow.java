59e2a37b2fadfc8d2bd10268b819e36c4c6fdbc8
Denis <deexort@mail.ru>
2025-05-22 21:59:12 +0300

    edit StorageWindow

--- src/main/java/view/StorageWindow.java
+++ src/main/java/view/StorageWindow.java
@@ -3,7 +3,7 @@ package view;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 
-public class StorageWindow {
+public class StorageWindow extends JComponent {
     private JPanel contentPane;
     private StorageTableModel storageTableModel;
     private JTable table1;
@@ -11,18 +11,22 @@ public class StorageWindow {
     private JButton addItem;
     private JButton deleteItem;
     private JButton editItem;
+    private JButton searchButton;
+    private JTextField searchField;
+    private JTextArea AplicabilityTextArea;
+    private JTable table2;
 
 
-    public StorageWindow() {
-        // Инициализация модели таблицы
-        DefaultTableModel model = new DefaultTableModel();
-        model.addColumn("Артикул");
-        model.addColumn("Наименование");
-        model.addColumn("Единица измерения");
-        model.addColumn("Описание");
-        model.addRow(new Object[]{1234567, "Продукт", "шт.", "описание продукта"});
-        table1.setModel(model);
-    }
+//    public StorageWindow() {
+//        // Инициализация модели таблицы
+//        DefaultTableModel model = new DefaultTableModel();
+//        model.addColumn("Артикул");
+//        model.addColumn("Наименование");
+//        model.addColumn("Единица измерения");
+//        model.addColumn("Описание");
+//        model.addRow(new Object[]{1234567, "Продукт", "шт.", "описание продукта"});
+//        table1.setModel(model);
+//    }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame();
